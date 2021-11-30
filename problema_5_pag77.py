28902a592163e779357975b2a4c7b9757219cd88
ana-mariatambur <71002457+ana-mariatambur@users.noreply.github.com>
2021-11-30 20:15:16 +0200

    Add files via upload

---
+++ problema_5_pag77.py
@@ -0,0 +1,4 @@
+with open('C:/Users/Steff/Documents/Trash/VS/Scoala/Cl XI/input.txt', 'r',encoding='utf8') as f: 
+    for i in f.readline():
+        if i in ['a','e','i','o','u','ă','î','â']:
+            print(i, end='')
