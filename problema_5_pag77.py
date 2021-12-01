cc9f6ec53ef8b2a4d40275f4713c388f4aa2b8d6
ana-mariatambur <71002457+ana-mariatambur@users.noreply.github.com>
2021-12-01 21:33:38 +0200

    Update problema_5_pag77.py

--- problema_5_pag77.py
+++ problema_5_pag77.py
@@ -1,4 +1,6 @@
-with open("input.txt","r",encoding="utf8") as f: 
+c=0
+with open("input.txt","r",encoding="utf8") as f:
     for i in f.readline():
         if i in ["a","e","i","o","u","ă","î","â"]:
-            with open("output.txt","a",encoding="utf8") as f2: f2.writelines(i)
+            c+=1
+with open("output.txt","a",encoding="utf8") as f2: f2.writelines(str(c))
